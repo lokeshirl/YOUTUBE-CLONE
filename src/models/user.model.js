d1a01f35a7f4f100968dbbb7660e6572428cad37
lokeshirl <lokeshkumarjha07+github@gmail.com>
2023-11-28 21:28:45 +0530

    configured cloudinary & multer - Add: user & video models - Add: utility class for cloudinary - Add: multer middleware for storing user avatar and coverImage

---
+++ src/models/user.model.js
@@ -0,0 +1,102 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  // only hashPassword when user password is modified
+  if (!this.isModified("password")) return next();
+
+  /**
+   * - hashing the user password before saving to DB
+   * - using npm bcrypt module
+   */
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashPassword;
+  next();
+});
+
+// method to verify if user's password is correct ?
+userSchema.methods.isPasswordCorrect = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+// method to generate access token - using jwt
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// method to generate refresh token - jwt
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const User = model("User", userSchema);
+
+export { User };
